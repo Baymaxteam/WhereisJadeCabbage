d6cce2db7e9a01757806bbf1766d13d963eedc2a
qwedsazxc78 <qwedsazxc78@gmail.com>
2017-11-14 21:19:41 +0800

    app ui prototype

---
+++ App_Heritage/app/src/main/java/qwedsazxc78/app_heritage/introActivity.java
@@ -0,0 +1,30 @@
+package qwedsazxc78.app_heritage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+public class introActivity extends AppCompatActivity {
+
+    String[] itemname ={
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
